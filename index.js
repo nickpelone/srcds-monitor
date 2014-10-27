518ff105fb874cb64926b46f0fe88305461ab1db
Nick Pelone <npelone@unca.edu>
2014-10-27 15:17:32 -0400

    Get info from srcds.nickpelone.com

---
+++ index.js
@@ -0,0 +1,27 @@
+/* jsint node:true */
+var express = require('express');
+var srcds = require('srcds-info');
+var http = require('http');
+var path = require('path');
+var app = express();
+
+var YOUR_SERVER_IP_HERE = 'srcds.nickpelone.com';
+var client = srcds(YOUR_SERVER_IP_HERE, 27015);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, function() {
+    console.log("Express app up on port 3000");
+});
+
+app.get("/servers.json", function(request, response) {
+    client.info(function (error, info) {
+        if(error) {
+            console.err(error);
+        } else {
+            response.json(info);
+            console.log(info);
+        }
+        client.close();
+    });
+});
